3be8adb6e2206b9d1807cbf22667795532bef845
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-15 17:33:41 -0500

    Sketch utxos

---
+++ src/p1_state_machine/p5_digital_cash.rs
@@ -0,0 +1,63 @@
+
+//! In this module we design another multi-user currency system. This one is not based on
+//! accounts, but rather, is modelled after a paper cash system. The system tracks individual
+//! cash bills. Each bill has an amount and an owner, and can be spent in its entirety.
+//! When a state transition spends bills, new bills are created in lesser or equal amount.
+
+use super::{StateMachine, User};
+use std::collections::HashSet;
+
+
+/// This state machine models a multi-user currency system. It tracks a set of bills in
+/// circulation, and updates that set when money is transferred.
+pub struct DigitalCashSystem;
+
+/// A single bill in the digital cash system. Each bill has an owner who is allowed to spent
+/// it and an amount that it is worth. It also has serial number to ensure that each bill
+/// is unique.
+pub struct Bill {
+    owner: User,
+    amount: u64,
+    serial: u64,
+}
+
+/// The State of a digital cash system. Primarily just the set of currently circulating bills.,
+/// but also a counter for the next serial number.
+pub struct State {
+    /// The set of currently circulating bills
+    bills: HashSet<Bill>,
+    /// The next serial number to use when a bill is created.
+    next_serial: u64,
+}
+
+/// The state transitions that users can make in a digital cash system
+pub enum CashTransaction {
+    /// Mint a single new bill owned by the minter
+    Mint {
+        minter: User,
+        amount: u64,
+    },
+    /// Send some money from some users to other users. The money does not all need
+    /// to come from the same user, and it does not all need to go to the same user.
+    /// The total amount received must be less than or equal to the amount spent.
+    /// The discrepancy between the amount sent and received is destroyed. Therefore,
+    /// no dedicated burn transaction is required.
+    Transfer {
+        spends: Vec<Bill>,
+        receives: Vec<Bill>,
+    }
+}
+
+/// We model this system as a state machine with two possible transitions
+impl StateMachine for DigitalCashSystem {
+
+    type State = State;
+    type Transition = CashTransaction;
+
+    fn next_state(starting_state: &State, t:&CashTransaction) -> State {
+        todo!("Exercise 1")
+    }
+}
+
+
+// TODO lots of tests
