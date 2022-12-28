6603419cbcf9f554f46afbdae8460d9baa6f5d53
Joshy Orndorff <admin@joshyorndorff.com>
2022-12-28 17:13:28 -0500

    Sketch tests for ATM state machine

--- src/p1_state_machine/p3_atm.rs
+++ src/p1_state_machine/p3_atm.rs
@@ -5,7 +5,7 @@
 use super::StateMachine;
 
 /// The keys on the ATM keypad
-#[derive(Hash)]
+#[derive(Hash, Debug, PartialEq, Eq)]
 pub enum Key {
     One,
     Two,
@@ -23,28 +23,99 @@ pub enum Action {
     PressKey(Key),
 }
 
+/// The various states of authentication possible with the ATM
+#[derive(Debug, PartialEq, Eq)]
+enum Auth {
+    /// No session has begun yet. Waiting for the user to swipe their card
+    Waiting,
+    /// The user has swiped their card, providing the enclosed PIN hash.
+    /// Waiting for the user to key in their pin
+    Authenticating(u64),
+    /// The user has authenticated. Waiting for them to key in the amount
+    /// of cash to withdraw
+    Authenticated,
+}
+
 /// The ATM. When a card is swiped, the ATM learns the correct pin's hash.
 /// It waits for you to key in your pin. You can press as many numeric keys as
 /// you like followed by enter. If the pin is incorrect, your card is returned
 /// and the ATM automatically goes back to the main menu. If your pin is correct,
 /// the ATM waits for you to key in an amount of money to withdraw. Withdraws
 /// are bounded only by the cash in the machine (there is no account balance).
+#[derive(Debug, PartialEq, Eq)]
 pub struct Atm {
     /// How much money is in the ATM
     cash_inside: u64,
-    /// The hash of a user's expected pin. The ATM will not expect any particular pin
-    /// before the user swipes their card.
-    expected_pin_hash: Option<u64>,
+    /// The machine's authentication status.
+    expected_pin_hash: Auth,
     /// All the keys that have been pressed since the last `Enter`
     keystroke_register: Vec<Key>,
 }
 
 impl StateMachine for Atm {
-    // Exercise: Fill in these associated types.
-    type State = ();
-    type Transition = ();
 
-    fn next_state(starting_state: &Self::State, t: &Self::Transition) -> () {
+    // Notice that we are using the same type for the state as we are using for the machine this time.
+    type State = Self;
+    type Transition = Action;
+
+    fn next_state(starting_state: &Self::State, t: &Self::Transition) -> Self::State {
         todo!()
     }
+}
+
+#[test]
+fn sm_3_simple_swipe_card() {
+    let start = Atm {
+        cash_inside: 10,
+        expected_pin_hash: Auth::Waiting,
+        keystroke_register: Vec::new(),
+    };
+    let end = Atm::next_state(&start, &Action::SwipeCard(1234));
+    let expected = Atm {
+        cash_inside: 10,
+        expected_pin_hash: Auth::Authenticating(1234),
+        keystroke_register: Vec::new(),
+    };
+
+    assert_eq!(end, expected);
+}
+
+#[test]
+fn sm_3_swipe_card_again_part_way_through() {
+    todo!()
+}
+
+#[test]
+fn sm_3_press_key_before_card_swipe() {
+    todo!()
+}
+
+#[test]
+fn sm_3_enter_single_digit_of_pin() {
+    todo!()
+}
+
+#[test]
+fn sm_3_enter_wrong_pin() {
+    todo!()
+}
+
+#[test]
+fn sm_3_enter_correct_pin() {
+    todo!()
+}
+
+#[test]
+fn sm_3_enter_single_digit_of_withdraw_amount() {
+    todo!()
+}
+
+#[test]
+fn sm_3_try_to_withdraw_too_much() {
+    todo!()
+}
+
+#[test]
+fn sm_3_withdraw_acceptable_amout() {
+    todo!()
 }
