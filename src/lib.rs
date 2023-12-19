8c5bcb2453706ba3f7fae7b81c948cdc5c312e21
Joshy Orndorff <admin@joshyorndorff.com>
2023-12-19 15:36:29 -0500

    rename chapter number to 4

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ use std::hash::{Hash, Hasher};
 mod c1_state_machine;
 mod c2_blockchain;
 mod c3_consensus;
-//TODO rename to c4
-mod c5_client;
+mod c4_client;
 
 // Simple helper to do some hashing.
 fn hash<T: Hash>(t: &T) -> u64 {
