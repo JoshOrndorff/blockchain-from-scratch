9f71df635ca60a1414d59b4bf434500542c06d31
Joshy Orndorff <admin@joshyorndorff.com>
2023-12-19 13:51:39 -0500

    Sketch idea for `HashValue` type to make it more clear which u64s are hash outputs.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ mod c2_blockchain;
 mod c3_consensus;
 mod c4_framework;
 
+//TODO This is just me scribbling a quick little idea to disambiguate the hash type throughout this project.
+/// A Simple 64-bit hash value that will be used whenever a cryptographic hash is needed.
+pub struct HashValue(u64);
+
 // Simple helper to do some hashing.
 fn hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
