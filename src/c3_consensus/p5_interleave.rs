a75b82b1682a487ddb8c8c3beee87578d2ac2251
Joshy Orndorff <admin@joshyorndorff.com>
2023-12-19 12:39:30 -0500

    finish solving c3p5 interleave

--- src/c3_consensus/p5_interleave.rs
+++ src/c3_consensus/p5_interleave.rs
@@ -3,16 +3,15 @@
 //! we could consider interleaving PoW blocks with PoA blocks. Some very early designs of Ethereum considered
 //! this approach as a way to transition away from PoW.
 
-use super::{Consensus, ConsensusAuthority, Header, p1_pow::Pow, p3_poa::SimplePoa};
+use super::{p1_pow::Pow, p3_poa::SimplePoa, Consensus, ConsensusAuthority, Header};
 
 /// A Consensus engine that alternates back and forth between PoW and PoA sealed blocks.
-/// 
+///
 /// Odd blocks are PoW
 /// Even blocks are PoA
 struct AlternatingPowPoa {
     // One approach to the fields is to store the pow threshold and poa authorities directly.
     // I think it is more elegant to store whole PoW instances that can be re-used.
-
     /// An instance of a PoW consensus that will be used for the PoW blocks
     pow: Pow,
     /// An instance of a PoA consensus that will be used for the PoA blocks
@@ -62,7 +61,7 @@ impl From<ConsensusAuthority> for PowOrPoaDigest {
 impl TryFrom<PowOrPoaDigest> for ConsensusAuthority {
     type Error = ();
 
-    fn try_from(d: PowOrPoaDigest) -> Result<Self, Self::Error>  {
+    fn try_from(d: PowOrPoaDigest) -> Result<Self, Self::Error> {
         match d {
             PowOrPoaDigest::Pow(_) => Err(()),
             PowOrPoaDigest::Poa(authority) => Ok(authority),
@@ -71,13 +70,49 @@ impl TryFrom<PowOrPoaDigest> for ConsensusAuthority {
 }
 
 impl Consensus for AlternatingPowPoa {
-    type Digest = ConsensusAuthority;
+    type Digest = PowOrPoaDigest;
 
     fn validate(&self, parent_digest: &Self::Digest, header: &Header<Self::Digest>) -> bool {
         // The requirement is based on evenness or oddness of the block height.
         if header.height % 2 == 0 {
-            self.poa.va
+            // TODO it may be nicer to have the From impls be on the header type as well
+            let poa_header = Header::<ConsensusAuthority> {
+                parent: header.parent,
+                height: header.height,
+                state_root: header.state_root,
+                extrinsics_root: header.extrinsics_root,
+                consensus_digest: header
+                    .consensus_digest
+                    .try_into()
+                    .expect("Even blocks should have PoA seal which can be downcast."),
+            };
+            let poa_parent_digest: ConsensusAuthority = parent_digest
+                .clone()
+                .try_into()
+                .expect("Even blocks should have PoA seal which can be downcast.");
+
+            self.poa.validate(&poa_parent_digest, &poa_header)
+        } else {
+            let pow_header = Header::<u64> {
+                parent: header.parent,
+                height: header.height,
+                state_root: header.state_root,
+                extrinsics_root: header.extrinsics_root,
+                consensus_digest: header
+                    .consensus_digest
+                    .try_into()
+                    .expect("Odd blocks should have PoW seal which can be downcast."),
+            };
+            let pow_parent_digest: u64 = parent_digest
+                .clone()
+                .try_into()
+                .expect("Odd blocks should have PoW seal which can be downcast.");
+
+            self.pow.validate(&pow_parent_digest, &pow_header)
         }
+
+        // If we wanted to be extra thorough, we could also check that we have the opposite seal
+        // type from the parent header. But really that is just checking that the parent was valid.
     }
 
     fn seal(
@@ -85,6 +120,36 @@ impl Consensus for AlternatingPowPoa {
         parent_digest: &Self::Digest,
         partial_header: Header<()>,
     ) -> Option<Header<Self::Digest>> {
-        todo!("Exercise 6")
+        if partial_header.height % 2 == 0 {
+            let poa_parent_digest: ConsensusAuthority = parent_digest
+                .clone()
+                .try_into()
+                .expect("Even blocks should have PoA seal which can be downcast.");
+
+            let poa_sealed_header = self.poa.seal(&poa_parent_digest, partial_header);
+
+            poa_sealed_header.map(|h| Header::<PowOrPoaDigest> {
+                parent: h.parent,
+                height: h.height,
+                state_root: h.state_root,
+                extrinsics_root: h.extrinsics_root,
+                consensus_digest: h.consensus_digest.into(),
+            })
+        } else {
+            let pow_parent_digest: u64 = parent_digest
+                .clone()
+                .try_into()
+                .expect("Odd blocks should have PoW seal which can be downcast.");
+
+            let pow_sealed_header = self.pow.seal(&pow_parent_digest, partial_header);
+
+            pow_sealed_header.map(|h| Header::<PowOrPoaDigest> {
+                parent: h.parent,
+                height: h.height,
+                state_root: h.state_root,
+                extrinsics_root: h.extrinsics_root,
+                consensus_digest: h.consensus_digest.into(),
+            })
+        }
     }
 }
