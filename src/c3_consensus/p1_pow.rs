13744ff23c42ee70e90b8accf16fb1811f3f1a1c
Joshy Orndorff <admin@joshyorndorff.com>
2023-12-19 12:10:15 -0500

    rename struct `PoW` -> `Pow`

--- src/c3_consensus/p1_pow.rs
+++ src/c3_consensus/p1_pow.rs
@@ -9,11 +9,11 @@ use super::{Consensus, Header};
 /// A Proof of Work consensus engine. This is the same consensus logic that we
 /// implemented in the previous chapter. Here we simply re-implement it in the
 /// consensus framework that will be used throughout this chapter.
-pub struct PoW {
+pub struct Pow {
     threshold: u64,
 }
 
-impl Consensus for PoW {
+impl Consensus for Pow {
     type Digest = u64;
 
     /// Check that the provided header's hash is below the required threshold.
@@ -31,12 +31,12 @@ impl Consensus for PoW {
 
 /// Create a PoW consensus engine that has a difficulty threshold such that roughly 1 in 100 blocks
 /// with randomly drawn nonces will be valid. That is: the threshold should be u64::max_value() / 100.
-pub fn moderate_difficulty_pow() -> PoW {
+pub fn moderate_difficulty_pow() -> Pow {
     todo!("Exercise 3")
 }
 
 /// Create an instance of the PoW Consensus that behaves identically to the trivial
 /// consensus implementation for `()` from the module level.
-pub fn trivial_always_valid_pow() -> PoW {
+pub fn trivial_always_valid_pow() -> Pow {
     todo!("Exercise 4")
 }
