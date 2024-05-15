57fb332bbc743ca86a2c9af28428714c3327bc7b
Ivan Subotic <400790+subotic@users.noreply.github.com>
2024-05-15 16:32:55 +0200

    add nix stuff

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustup
+          clang
+          protobuf
+        ] ++ (pkgs.lib.optional stdenv.isDarwin [
+          pkgs.iconv
+          darwin.apple_sdk.frameworks.Foundation
+        ]);
+
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+      };
+    });
+}
