51b5b05aee47e632e0aa18bc163cf62850e2957e
Martin Barreto <mtn.barreto@gmail.com>
2024-05-27 01:42:31 -0300

    p1 switches state machine

--- src/c1_state_machine/p1_switches.rs
+++ src/c1_state_machine/p1_switches.rs
@@ -14,8 +14,8 @@ impl StateMachine for LightSwitch {
     type State = bool;
     type Transition = ();
 
-    fn next_state(starting_state: &bool, t: &()) -> bool {
-        todo!("Exercise 1")
+    fn next_state(starting_state: &bool, _t: &()) -> bool {
+        !starting_state
     }
 }
 
@@ -42,7 +42,29 @@ impl StateMachine for WeirdSwitchMachine {
     type Transition = Toggle;
 
     fn next_state(starting_state: &TwoSwitches, t: &Toggle) -> TwoSwitches {
-        todo!("Exercise 2")
+        match t {
+            Toggle::FirstSwitch => {
+                if starting_state.first_switch {
+                    // Turning off the first switch, so also turn off the second switch
+                    TwoSwitches {
+                        first_switch: false,
+                        second_switch: false,
+                    }
+                } else {
+                    // Turning on the first switch
+                    TwoSwitches {
+                        first_switch: true,
+                        second_switch: starting_state.second_switch,
+                    }
+                }
+            }
+            Toggle::SecondSwitch => {
+                TwoSwitches {
+                    first_switch: starting_state.first_switch,
+                    second_switch: !starting_state.second_switch,
+                }
+            }
+        }
     }
 }
 
